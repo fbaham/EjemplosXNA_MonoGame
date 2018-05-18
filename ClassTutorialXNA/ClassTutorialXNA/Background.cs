826e167a6839ac64889301c07db77b05cedfb222
Francisco Bahamondes <francisco.bahamondes@hipodromo.local>
2018-05-18 16:44:30 -0400

    first commit

---
+++ ClassTutorialXNA/ClassTutorialXNA/Background.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTutorialXNA
+{
+    class Background
+    {
+        public Texture2D texture;
+        public Rectangle rectangle;
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, rectangle, Color.White);
+        }
+    }
+
+    class Scrolling : Background
+    {
+        public Scrolling(Texture2D newTexture, Rectangle newRectange)
+        {
+            texture = newTexture;
+            rectangle = newRectange;
+        }
+
+        public void Update()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                rectangle.X -= 3;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                rectangle.X += 3;
+
+        }
+
+    }
+}
---
+++ ClassTutorialXNA/ClassTutorialXNA/Game1.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ClassTutorialXNA
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Scrolling sky;
+        Scrolling rocksBack1;
+        Scrolling rocksFront1;
+
+        Scrolling rocksBack2;
+        Scrolling rocksFront2;
+
+        // Game World
+        Sprite luke, lukeBlue;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            sky = new Scrolling(Content.Load<Texture2D>("layers/sky"), new Rectangle(0, 0, 800, 480));
+
+            rocksBack1 = new Scrolling(Content.Load<Texture2D>("layers/rocks_1"), new Rectangle(0, 0, 800, 480));
+            rocksFront1 = new Scrolling(Content.Load<Texture2D>("layers/rocks_2"), new Rectangle(0, 0, 800, 480));
+
+            rocksBack2 = new Scrolling(Content.Load<Texture2D>("layers/rocks_1"), new Rectangle(800, 0, 800, 480));
+            rocksFront2 = new Scrolling(Content.Load<Texture2D>("layers/rocks_2"), new Rectangle(800, 0, 800, 480));
+
+            luke = new Character(Content.Load<Texture2D>("Luke_idle"), new Rectangle(10,400, Content.Load<Texture2D>("Luke_idle").Width, Content.Load<Texture2D>("Luke_idle").Height),GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            
+
+            if (rocksBack1.rectangle.X + rocksBack1.rectangle.Width <= 0)
+                rocksBack1.rectangle.X = rocksBack2.rectangle.X + rocksBack2.rectangle.Width;
+
+            if (rocksBack2.rectangle.X + rocksBack2.rectangle.Width <= 0)
+                rocksBack2.rectangle.X = rocksBack1.rectangle.X + rocksBack1.rectangle.Width;
+
+            if (rocksFront1.rectangle.X + rocksFront1.rectangle.Width <= 0)
+                rocksFront1.rectangle.X = rocksFront2.rectangle.X + rocksFront2.rectangle.Width;
+
+            if (rocksFront2.rectangle.X + rocksFront2.rectangle.Width <= 0)
+                rocksFront2.rectangle.X = rocksFront1.rectangle.X + rocksFront1.rectangle.Width;
+
+            luke.Update();
+
+            if (luke.rectangle.X >= 400)
+            {
+                rocksBack1.Update();
+                rocksFront1.Update();
+
+                rocksBack2.Update();
+                rocksFront2.Update();
+            }
+            
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            sky.Draw(spriteBatch);
+
+            rocksBack1.Draw(spriteBatch);
+            rocksFront1.Draw(spriteBatch);
+
+            rocksBack2.Draw(spriteBatch);
+            rocksFront2.Draw(spriteBatch);
+
+            luke.Draw(spriteBatch);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
